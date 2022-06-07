1fb59fecbe9f677056102fda137b0e2ad95e1a4e
Ankit Dewangan <101589446+ankitdew05@users.noreply.github.com>
2022-06-07 23:22:36 +0530

    user profile solution

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,6 +1,6 @@
 const expressJwt = require('express-jwt');
 
-function authJwt(){
+function authJwt() {
     const secret = process.env.secret;
     const api = process.env.API_URL;
     return expressJwt({
@@ -8,23 +8,26 @@ function authJwt(){
         algorithms: ['HS256'],
         isRevoked: isRevoked
     }).unless({
-        path:[
-            {url: /\/public\/uploads(.*)/, methods: ['GET', 'OPTIONS']},
-            {url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS']},
-            {url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS']},
+        path: [
+            {url: /\/public\/uploads(.*)/ , methods: ['GET', 'OPTIONS'] },
+            {url: /\/api\/v1\/users\/.*/,methods: ['GET']},
+            {url: /\/api\/v1\/products(.*)/ , methods: ['GET', 'OPTIONS'] },
+            {url: /\/api\/v1\/categories(.*)/ , methods: ['GET', 'OPTIONS'] },
+            {url: /\/api\/v1\/orders(.*)/,methods: ['GET', 'OPTIONS', 'POST']},
             `${api}/users/login`,
-            `${api}/users/register`
-
+            `${api}/users/register`,
         ]
     })
 }
 
-async function isRevoked(req , payload , done){
-    if(!payload.isAdmin){
-        done(null , true)
+async function isRevoked(req, payload, done) {
+    if(!payload.isAdmin) {
+        done(null, true)
     }
-    
+
     done();
 }
 
-module.exports = authJwt;
+
+
+module.exports = authJwt
